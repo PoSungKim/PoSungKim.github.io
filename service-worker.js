a414d7660fb6304ee015aa9c0d1b8836e6aea912
PoSungKim <psungkr94@likelion.org>
2020-09-02 14:22:45 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f1879b467512394a6726c8dea50d670.js"
+  "/precache-manifest.db6f0190442f29ad607db0f5c462ea7f.js"
 );
 
 self.addEventListener('message', (event) => {
